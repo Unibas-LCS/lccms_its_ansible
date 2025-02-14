71c36b56507bb4ece5dca154ec1311b9b748c693
Root user <root-dmi@unibas.ch>
2025-02-14 16:34:58 +0100

    ansibleRun same as 24.04

--- ansible/templates/ansibleRun.sh
+++ ansible/templates/ansibleRun.sh
@@ -13,7 +13,7 @@ HOST={{ lccms.host }}
 UNIT={{ lccms.unit }}
 LCCMSOS={{ lccms.os | default('') }}
 LCCMSRELEASE={{ lccms.release | default('') }}
-LCCMSCONFIGURATION={{ lccms.configuration }}   # managed, self-managed
+LCCMSCONFIGURATION={{ lccms.configuration }}   # managed, self-managed, unmanaged
 MACHINESTATE={{ lccms.status }}  # active, retired, ...
 
 LOGFILE=/var/log/lccmsrun.log
@@ -112,35 +112,86 @@ do
   s=`echo $r | /usr/bin/sed 's/\./\//'`
   d=`/usr/bin/dirname $s`
   /usr/bin/wget -q -N -e robots=off --timestamping --no-parent -r -l 8 -nH --cut-dirs=3 -R '*.html*' --execute='robots = off' ${ROLESURI}${LCOS}/${LCRELEASE}/${s}/
-  /usr/bin/ln -sf ${s} ${r}
+  /usr/bin/ln -sfn ${s} ${r}
 done
 out "done."
 cd ..
 
 out ""
-out "Running ansible."
-
 TAG=""
-if [[ "$LCCMSCONFIGURATION" = "self-managed" ]] # Only execute commands with the tag 'self-managed'!
+if [[ "$LCCMSCONFIGURATION" != "managed"  || "$MACHINESTATE" == "retired" ]] # Only execute commands with the tag 'self-managed'!
 then
-  echo "THIS MACHINE IS SELF MANAGED!"  >>$LOGFILE
-  TAG='--tags "self-managed"'
+  if [[ "$LCCMSCONFIGURATION" == "self-managed" ]]
+  then
+    echo "THIS MACHINE IS SELF MANAGED!"  >>$LOGFILE
+  else
+    echo "THIS MACHINE HAS BEEN UNCONFIGURED!"  >>$LOGFILE
+  fi
+  TAG='--tags self-managed'
 fi
-
-if [[ "$MACHINESTATE" == "retired" ]]
-then
-  echo "THIS MACHINE HAS BEEN RETIRED!"  >>$LOGFILE
-fi
-
+out "Running ansible."
 # Set the log file, then run ansible normally in order to get the colours.
 /usr/bin/rm -f ~/ansible.log
 export ANSIBLE_LOG_PATH=~/ansible.log
 # Now run the playbook. Save the output to a file and also show on screen.
-/usr/bin/ansible-playbook ${HOST}.yml
+/usr/bin/ansible-playbook ${HOST}.yml $TAG
 # Cat the log file to the existing log file.
 /usr/bin/sed 's/[^|]*| //' ~/ansible.log >>$LOGFILE
 /usr/bin/rm ~/ansible.log
->>>>>>> 542f8a02bf5b0fca8b73c5c47a4b10d0bb26f5b6
+
+if [[ "$MACHINESTATE" == "retired" || "$LCCMSCONFIGURATION" == "unmanaged" ]]
+then
+  out "The lccms configutaions will be removed from this system."
+  out "All users listed in the home directory will have an entry in /etc/passwd and any external authentication will be disabled/removed."
+  out "Users which were not in /etc/passwd will have their password set to their login name."
+  echo "STATUS: Ensure all users have an entry in /etc/passwd." >>$LOGFILE
+  # Check that all users are in /etc/passwd:
+  for f in /home/*
+  do
+    uid=`/usr/bin/basename $f`
+    if ! /usr/bin/grep $uid /etc/passwd >/dev/null
+    then
+      user=`/usr/bin/getent -s sss passwd $uid`
+      if [[ $? == 0 ]]
+      then
+        out "Adding user $uid to /etc/passwd with password $uid."
+        echo $user >>/etc/passwd
+        pw=`echo $uid | /usr/bin/mkpasswd --method=SHA-512 --stdin`
+        days=`echo $(( $(date +%s) / 86400 ))`
+        echo "$uid:$pw:$days:0:99999:7:::" >>/etc/shadow
+      else
+        out "Could not get the password entry for user $uid."
+      fi
+    fi
+  done
+  echo "STATUS END" >>$LOGFILE
+  echo "STATUS: Remove any directory usage." >>$LOGFILE
+  # Remove any sssd from /etc/nsswitch.conf (sss ) and pam (pam_sss.so)
+  out "Removing sssd usage from nsswitch, if configured."
+  /usr/bin/sed -i '/sss/s/sss//' /etc/nsswitch.conf
+  for f in `/usr/bin/grep -l pam_sss.so /etc/pam.d/*`
+  do
+    out "Removing sssd from pam module $f."
+    /usr/bin/sed -i '/pam_sss.so/d' $f
+  done
+  echo "STATUS END" >>$LOGFILE
+  echo "STATUS: Remove the management software." >>$LOGFILE
+  # Remove any system service and timer entries.
+  out "Removing timer entry."
+  /usr/bin/systemctl stop ansible.timer
+  /usr/bin/systemctl disable ansible.timer
+  /usr/bin/systemctl stop ansible.service
+  /usr/bin/systemctl disable ansible.service
+  # Remove all roles
+  cd /usr/.ansible
+  /usr/bin/rm -rf roles
+  /usr/bin/rm -rf *.yml
+  # unlink this file
+  /usr/bin/unlink ansibleRun.sh
+  cd ../
+  /usr/bin/rmdir .ansible
+  echo "STATUS END" >>$LOGFILE
+fi
 
 # We will write an HTML file on what was done into $ACTIONDIR/actions.html
 # Check that the path exists:
