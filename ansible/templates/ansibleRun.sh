6271343a27d851b2bc964b131b0fa0c0215399c5
root <root@server>
2025-05-18 18:52:41 +0200

    more on user info

--- ansible/templates/ansibleRun.sh
+++ ansible/templates/ansibleRun.sh
@@ -237,6 +237,9 @@ response=$(/usr/bin/curl -s -o /tmp/response.html -w '%{response_code}' --form "
 if [[ $? == 0 && $response == "200" ]]
 then
   /bin/mv -f /tmp/response.html ${ACTIONDIR}/actions.html
+  cd ${ACTIONDIR}
+  /usr/bin/ln actions.html actions_en.html
+  /usr/bin/ln actions.html actions_de.html
 else
   /bin/rm -f /tmp/response.html
 fi
